a37cec8568292d991f7365bae35301a55325aec3
Paulo Garces <prgar6@gmail.com>
2026-05-11 01:35:43 -0300

    fix: ajusta rolagem e fundo de serras na home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -104,11 +104,11 @@ export default function Home() {
   const shortcuts = popularSearches.length > 0 ? popularSearches : defaultShortcuts;
 
   return (
-    <div className="h-screen w-full overflow-hidden bg-white relative flex flex-col justify-center items-center">
+    <div className="h-[100dvh] w-full overflow-hidden bg-white relative flex flex-col justify-center items-center">
       
       {/* Wave Background (Serras - Múltiplas camadas de verde) */}
-      <div className="absolute bottom-0 left-0 w-full z-10 flex flex-col justify-end overflow-hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full h-auto min-w-[200%] md:min-w-full -ml-[50%] md:ml-0 translate-y-2">
+      <div className="absolute bottom-0 left-0 w-full h-[30vh] min-h-[200px] z-10 pointer-events-none">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full h-full" preserveAspectRatio="xMidYMax slice">
           {/* Camada de fundo (mais clara/alta) */}
           <path fill="#34d399" fillOpacity="0.4" d="M0,224L48,202.7C96,181,192,139,288,149.3C384,160,480,224,576,218.7C672,213,768,139,864,133.3C960,128,1056,192,1152,213.3C1248,235,1344,213,1392,202.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           {/* Camada intermediária */}
